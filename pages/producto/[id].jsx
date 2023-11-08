e98a66859f3ab228b575c4ee068549fb74a0f2db
Pablo Levy <114173733+pabloele@users.noreply.github.com>
2023-11-08 08:35:24 -0300

    text select not alowed in line

--- pages/producto/[id].jsx
+++ pages/producto/[id].jsx
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from "react";
-import TrazabilityLine from "../../components/TrazabilityLine/TrazabilityLine";
-import { HomeLayout } from "../../layout";
-import { Box, Typography, IconButton, Tab, Tabs, Button } from "@mui/material";
-import useProduct from "../../hooks/useProduct";
-import { useRouter } from "next/router";
-import Modal from "@mui/material/Modal";
-import { AddOutlined } from "@mui/icons-material";
-import Trazability from "../../components/Trazability/Trazability";
-import TabPanel from "../../components/TabPanel/TabPanel";
-import useMilestone from "../../hooks/useMilestone";
-import QRCode from "qrcode";
-import Image from "next/image";
+import React, { useState, useEffect } from 'react';
+import TrazabilityLine from '../../components/TrazabilityLine/TrazabilityLine';
+import { HomeLayout } from '../../layout';
+import { Box, Typography, IconButton, Tab, Tabs, Button } from '@mui/material';
+import useProduct from '../../hooks/useProduct';
+import { useRouter } from 'next/router';
+import Modal from '@mui/material/Modal';
+import { AddOutlined } from '@mui/icons-material';
+import Trazability from '../../components/Trazability/Trazability';
+import TabPanel from '../../components/TabPanel/TabPanel';
+import useMilestone from '../../hooks/useMilestone';
+import QRCode from 'qrcode';
+import Image from 'next/image';
 const Producto = () => {
   const router = useRouter();
 
@@ -30,13 +30,13 @@ const Producto = () => {
 
   const handleOpen = () => {
     setTabActive(0);
-    setSubprocessSelected(null)
+    setSubprocessSelected(null);
     setOpen(true);
   };
 
   const handleClose = () => setOpen(false);
   const handleClickSubprocess = (event) => {
-    const subprocess = event.target.getAttribute("name");
+    const subprocess = event.target.getAttribute('name');
     setSubprocessSelected(subprocess);
   };
   const handleChange = (event, newValue) => {
@@ -47,7 +47,7 @@ const Producto = () => {
     let milestonesValid = true;
 
     milestones.forEach((element, index) => {
-      if (element.image === "" || element.description === "") {
+      if (element.image === '' || element.description === '') {
         const number = index + 1;
         alert(`Faltan completar datos en el hito número ${number}`);
         milestonesValid = false;
@@ -59,7 +59,7 @@ const Producto = () => {
     }
 
     if (!subprocessSelected || tabActive === null) {
-      alert("Por favor, selecciona un proceso y un subproceso.");
+      alert('Por favor, selecciona un proceso y un subproceso.');
       return;
     }
 
@@ -81,8 +81,8 @@ const Producto = () => {
 
       // Restablecer estados y cerrar el modal
       setMilestoneBox([0]);
-      setMilestones([{ description: "", image: "" }]);
-      setFileUri("");
+      setMilestones([{ description: '', image: '' }]);
+      setFileUri('');
       setSubprocessSelected(null);
       setTabActive(null);
       setOpen(false); // Cierra el modal
@@ -94,33 +94,33 @@ const Producto = () => {
   const createQRcode = () => {
     const QRdata = `${process.env.NEXT_PUBLIC_PAGE_URL}/history/${router.query.id}`;
 
-    const canvas = document.getElementById("canvas");
+    const canvas = document.getElementById('canvas');
 
     QRCode.toCanvas(canvas, QRdata, async function (err, url) {
-      const qrUrl = canvas.toDataURL("image/png");
+      const qrUrl = canvas.toDataURL('image/png');
 
       const response = await uploadQr(product, qrUrl);
     });
   };
 
   const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: "80vw",
-    height: "90vh",
-    overflowY: "auto", // Habilita el desplazamiento vertical
-    bgcolor: "background.paper",
-    border: "2px solid #000",
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '80vw',
+    height: '90vh',
+    overflowY: 'auto',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
     boxShadow: 24,
     p: 4,
-    textAlign: "center",
+    textAlign: 'center',
   };
   if (!product) {
     return (
       <HomeLayout>
-        <Box container sx={{ height: "90vh" }}>
+        <Box container sx={{ height: '90vh' }}>
           <p>Loading...</p>
         </Box>
       </HomeLayout>
@@ -133,7 +133,7 @@ const Producto = () => {
             <Box>
               <Typography
                 sx={{
-                  color: "primary.main",
+                  color: 'primary.main',
                   fontSize: 24,
                 }}
               >
@@ -148,7 +148,7 @@ const Producto = () => {
                   <Tab
                     label={element.name}
                     sx={{
-                      color: "primary.main",
+                      color: 'primary.main',
                     }}
                     key={element.name}
                   />
@@ -158,7 +158,7 @@ const Producto = () => {
             {product.trazability.map((element, index) => (
               <Box key={element.name}>
                 <TabPanel
-                  sx={{ display: "flex", flexDirection: "row", gap: 2 }}
+                  sx={{ display: 'flex', flexDirection: 'row', gap: 2 }}
                   value={tabActive}
                   index={index}
                   key={index}
@@ -170,9 +170,9 @@ const Producto = () => {
                         marginTop: 1,
                         backgroundColor:
                           subprocessSelected === subprocess.name
-                            ? "primary.main"
-                            : "transparent",
-                        transition: "background-color 0.3s ease",
+                            ? 'primary.main'
+                            : 'transparent',
+                        transition: 'background-color 0.3s ease',
                       }}
                     >
                       <Typography
@@ -181,13 +181,13 @@ const Producto = () => {
                         sx={{
                           color:
                             subprocessSelected === subprocess.name
-                              ? "white"
-                              : "primary.main",
+                              ? 'white'
+                              : 'primary.main',
                           marginY: 2,
                           fontSize: 12,
-                          textTransform: "uppercase",
-                          ":hover": {
-                            cursor: "pointer",
+                          textTransform: 'uppercase',
+                          ':hover': {
+                            cursor: 'pointer',
                           },
                         }}
                       >
@@ -217,13 +217,13 @@ const Producto = () => {
         <Box>
           <Typography
             sx={{
-              color: "primary.main",
+              color: 'primary.main',
               fontSize: 24,
             }}
           >
             Cadena de produccion para : {product.name}
           </Typography>
-          <Box sx={{ display: "flex" }}>
+          <Box sx={{ display: 'flex' }}>
             <TrazabilityLine protocol={product.trazability} />
             {!product?.qrcode && <canvas id="canvas"></canvas>}
             <Image
@@ -234,7 +234,7 @@ const Producto = () => {
             />
           </Box>
 
-          <Box sx={{ display: "flex", gap: 2 }}>
+          <Box sx={{ display: 'flex', gap: 2 }}>
             <Button
               variant="contained"
               onClick={createQRcode}
@@ -248,10 +248,10 @@ const Producto = () => {
         <IconButton
           size="large"
           sx={{
-            color: "white",
-            backgroundColor: "error.main",
-            ":hover": { backgroundColor: "error.main", opacity: 0.9 },
-            position: "fixed",
+            color: 'white',
+            backgroundColor: 'error.main',
+            ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+            position: 'fixed',
             right: 50,
             bottom: 50,
           }}
