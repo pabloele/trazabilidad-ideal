fe54094ad58ca73f6f938b6a216d1b9d34b34242
nachito02 <arguellojuan08@gmail.com>
2023-11-10 23:29:05 -0300

    modificando vista para agregar productos

--- hooks/useMilestone.jsx
+++ hooks/useMilestone.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
+import IPFS from "ipfs-mini";
+
 const useMilestone = () => {
   const [milestones, setMilestones] = useState([
     { image: "", description: "" },
@@ -10,6 +12,14 @@ const useMilestone = () => {
   const { uploadFile, getFile } = useAuth();
 
   const handleImageUpload = async (index) => {
+    const ipfs = new IPFS({
+      host: "ipfs.infura.io",
+      port: 5001,
+      protocol: "https",
+      key: "2UrrpLL3Im3ATvqSRLI8EEwB25F",
+      secret: "b9678eacd899ae478394e33bf2aa602d",
+    });
+
     try {
       const input = document.createElement("input");
       input.type = "file";
@@ -18,24 +28,38 @@ const useMilestone = () => {
       input.onchange = async (e) => {
         const file = e.target.files[0];
         if (file) {
-          const response = await uploadFile(file);
-          const firebaseFullPath = response.metadata.fullPath;
-          const urlImage = await getFile(firebaseFullPath);
+          const reader = new FileReader();
 
-          setFileUri((prevFileUri) => {
-            const newFileUri = [...prevFileUri];
-            newFileUri[index] = urlImage;
-            return newFileUri;
-          });
+          reader.onloadend = async () => {
+            const buffer = Buffer.from(reader.result);
 
-          // Establece el valor de image en el milestone actual
-          setMilestones((prevMilestones) => {
-            const newMilestones = [...prevMilestones];
-            newMilestones[index].image = urlImage;
-            return newMilestones;
-          });
+            // Sube el archivo a IPFS
+            ipfs.add(buffer, async (err, result) => {
+              if (err) {
+                console.error("Error al subir el archivo a IPFS:", err);
+              } else {
+                const ipfsHash = result;
+                const urlImage = `https://ipfs.infura.io/ipfs/${ipfsHash}`;
 
-          console.log(milestones);
+                setFileUri((prevFileUri) => {
+                  const newFileUri = [...prevFileUri];
+                  newFileUri[index] = urlImage;
+                  return newFileUri;
+                });
+
+                // Establece el valor de image en el milestone actual
+                setMilestones((prevMilestones) => {
+                  const newMilestones = [...prevMilestones];
+                  newMilestones[index].image = urlImage;
+                  return newMilestones;
+                });
+
+                console.log(milestones);
+              }
+            });
+          };
+
+          reader.readAsArrayBuffer(file);
         }
       };
 
@@ -44,7 +68,6 @@ const useMilestone = () => {
       console.error("Error al subir la imagen:", error);
     }
   };
-
 
   return {
     setFileUri,
