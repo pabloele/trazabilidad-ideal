4032bed513f0934dbd91548d5315795e319f7f9f
Pablo Levy <114173733+pabloele@users.noreply.github.com>
2024-06-13 10:08:29 -0300

    back button position

--- components/Buttons/BackButton.jsx
+++ components/Buttons/BackButton.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import { Button, Box } from '@mui/material';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { useRouter } from 'next/router';
+import React from "react";
+import { Button, Box, useMediaQuery } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useRouter } from "next/router";
 
 const BackButton = () => {
   const router = useRouter();
-  if (router.pathname === '/home' || router.pathname === '/nueva-produccion') {
+  const isNotSmallScreen = useMediaQuery("(min-width: 600px)");
+  if (router.pathname === "/home" || router.pathname === "/nueva-produccion") {
     return null;
   }
   return (
     <ArrowBackIcon
       sx={{
         fontSize: 40,
-        position: 'fixed',
-        top: 160,
-        left: 20,
-        color: 'gray',
-        boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+        position: "fixed",
+        top: isNotSmallScreen ? 80 : 65,
+        left: isNotSmallScreen ? 20 : 10,
+        color: "gray",
+        boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
         borderRadius: 8,
-        cursor: 'pointer',
+        cursor: "pointer",
       }}
       onClick={() => router.back()}
     />
